4dc5ed7ec2eaafb171d9a63c79170612f832538e
Samsung <arslan.zakirov.1996@mail.ru>
2023-03-07 21:32:01 +0300

    первый коммит!

---
+++ src/test/java/com/example/workwithspringandmockito/WorkWithSpringAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workwithspringandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWithSpringAndMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
